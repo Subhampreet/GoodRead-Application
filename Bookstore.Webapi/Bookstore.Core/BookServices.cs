69acd5116cffc551b30de577c056670a5d914318
Subhampreet <subhampreet18@gmail.com>
2022-04-03 22:16:44 +0530

    File Restructuring

---
+++ Bookstore.Webapi/Bookstore.Core/BookServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bookstore.Core
+{
+    public class BookServices : IBookServices
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    Name = "Harry Potter",
+                    Price = 12.99
+                }
+            };
+        }
+    }
+}
---
+++ Bookstore.Webapi/Bookstore.Webapi/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using Bookstore.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Webapi.Controllers
+{
+    [Route("books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookServices _bookServices;
+        public BooksController(IBookServices bookServices)
+        {
+            _bookServices = bookServices;
+        }
+        [HttpGet]
+        [Route("getbooks/")]
+        public IActionResult GetBooks()
+        {
+            return Ok(_bookServices.GetBooks());
+        }
+    }
+}
