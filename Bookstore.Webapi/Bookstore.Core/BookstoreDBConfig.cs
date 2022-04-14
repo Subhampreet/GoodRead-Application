942094dd8aa61c76e7f291886bd03ccdf3791e7e
Subhampreet <subhampreet18@gmail.com>
2022-04-14 14:56:42 +0530

    BookstoreDBConfig Initialised

---
+++ Bookstore.Webapi/Bookstore.Core/BookstoreDBConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Core
+{
+    class BookstoreDBConfig
+    {
+        public string Database_Name { get; set; }
+        public string Books_Collection_Name { get; set; }
+        public string Connection_String { get; set; }
+
+    }
+}
