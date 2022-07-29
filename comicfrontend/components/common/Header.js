9dd1b992bd2705425c7495ec658c789a2011f10d
Subhampreet <subhampreet18@gmail.com>
2022-07-30 00:19:41 +0530

    🦊Header Completed

---
+++ comicfrontend/components/common/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+import { GiAries } from "react-icons/gi";
+import { CgMenuGridO } from "react-icons/cg";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="main">
+        <div className="body">
+          <div className="logo">
+            <img src="./logo2.png" className="logo-img" />
+          </div>
+          
+          <div className="right">
+            <div className="search">
+                <input type="text" placeholder="Search Manga" />
+                <button className="searchbutton">SEARCH</button>
+                {/* <FiSearch /> */}
+            </div>
+            <div className="account">
+              <GiAries className="icon" />
+              <CgMenuGridO className="icon" />
+              <div className="account">
+                <img src="https://avatarfiles.alphacoders.com/209/209229.jpg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
