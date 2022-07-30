b7ad32b0080ab0b31c7aceb8a0c744db4bbff8b5
Subhampreet <subhampreet18@gmail.com>
2022-07-30 19:28:59 +0530

    Bug: Deployment Issue Resolve Trial -2

--- comicfrontend/pages/index.js
+++ comicfrontend/pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600&family=Roboto:ital,wght@0,100;0,300;0,500;0,700;1,100;1,300&display=swap"
           rel="stylesheet"
         />
       </Head>
